2a51897befa9c67b4a297d7f1b90f66d765ac441
max <max.haeussler@gmail.com>
2024-04-09 19:38:59 +0200

    added cluster class, improved clustering data structure

--- pyeed/containers/mmseqs2.py
+++ pyeed/containers/mmseqs2.py
@@ -1,18 +1,13 @@
 import logging
 import random
 import os
-import re
 import string
 from typing import List
-from pathlib import Path
 from docker.models.containers import Container
 from pydantic import Field
 from pyeed.containers import AbstractContainer, ToolImage
+from pyeed.containers.cluster import Cluster
 from pyeed.core.abstractsequence import AbstractSequence
-
-# path_config = Path(__file__).parent.parent.parent / "logging.conf"
-# logging.config.fileConfig(path_config)
-# logger = logging.getLogger("pyeed")
 
 logger = logging.getLogger(__name__)
 
@@ -118,6 +113,11 @@ class MMSeqs2(AbstractContainer):
         le=3,
     )
 
+    clusters: list[Cluster] = Field(
+        description="The clusters of sequences",
+        default=None,
+    )
+
     def create_file(self, data: str):
         """
         Sets up the input data for the MMSeqs2 container.
@@ -189,7 +189,22 @@ class MMSeqs2(AbstractContainer):
         alignment_mode: int = 3,
         alignment_output_mode: int = 0,
         cluster_mode: int = 0,
-    ) -> List[AbstractSequence]:
+    ):
+        """
+        Clusters a list of sequences using the MMSeqs2 tool.
+
+        Args:
+            sequences (List[AbstractSequence]): A list of sequences to be clustered.
+            identity (float, optional): Minimum sequence identity threshold for clustering. Value should be between 0 and 1. Default is 0.
+            coverage (float, optional): Minimum fraction of aligned (covered) residues to consider a match. Value should be between 0 and 1. Default is 0.8.
+            cov_mode (int, optional): Defines the coverage mode used in comparisons. Ranges from 0 to 5. Default is 0.
+            alignment_mode (int, optional): Specifies how the alignment is computed. Ranges from 0 to 4. Default is 3.
+            alignment_output_mode (int, optional): Specifies the alignment output mode. Ranges from 0 to 5. Default is 0.
+            cluster_mode (int, optional): Determines the clustering strategy to be used. Ranges from 0 to 3. Default is 0.
+
+        Returns:
+            MMSeqs2: An instance of the MMSeqs2 class with the clusters stored in the 'clusters' attribute.
+        """
 
         mode = "easy-cluster"
         instance = cls(
@@ -209,23 +224,62 @@ class MMSeqs2(AbstractContainer):
 
         result = instance.run_container(command, multifasta)
 
-        ids = re.findall(r">([A-Z0-9\.]+) ", result["representatives"])
+        clusters = instance._map_clusters(sequences, result)
 
-        return list(filter(lambda x: x.source_id in ids, sequences))
+        print(
+            f"🎉 Clustered intitial sequences in {len(clusters)} representative sequences"
+        )
+
+        cls.clusters = clusters
+
+        return cls
+
+    def _map_clusters(self, sequences: List[AbstractSequence], result: dict):
+        """
+        Maps the clusters obtained from the MMSeqs2 result to the corresponding sequences.
+
+        Args:
+            sequences (List[AbstractSequence]): The list of all sequences used in the clustering.
+            result (dict): The result dictionary obtained from the MMSeqs2 container.
+
+        Returns:
+            List[Cluster]: The list of clusters, where each cluster contains a representative sequence and its members.
+        """
+
+        clusters = []
+        for representative in result["representatives"]:
+            members = []
+            cluster = Cluster(
+                representative=next(
+                    filter(lambda x: x.source_id == representative, sequences)
+                )
+            )
+            for item in result["cluster"]:
+                if item[0] == representative:
+                    members.append(
+                        next(filter(lambda x: x.source_id == item[1], sequences))
+                    )
+
+            cluster.members = members
+            clusters.append(cluster)
+
+        # sort clusters by decreasing member size
+        clusters.sort(key=lambda x: len(x.members), reverse=True)
+
+        return clusters
 
     def extract_output_data(self):
         results = {}
         with open(f"{self._tempdir_path}/result_rep_seq.fasta") as f:
-            results["representatives"] = f.read()
+            results["representatives"] = [
+                id.strip()[1:] for id in f.read().split("\n") if id.startswith(">")
+            ]
 
         with open(f"{self._tempdir_path}/result_cluster.tsv") as f:
-            results["cluster"] = f.read()
-
-        n_clusters = results["representatives"].count(">")
+            results["cluster"] = [
+                tuple(item.split("\t")) for item in f.read().strip().split("\n")
+            ]
 
         self._delete_temp_dir()
-        print(
-            f"🎉 Clustered intitial sequences in {n_clusters} representative sequences"
-        )
 
         return results
