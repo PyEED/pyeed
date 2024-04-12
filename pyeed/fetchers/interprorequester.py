5d46adc4243f3e62e2935697053cc16e1a6e4ea8
max <max.haeussler@gmail.com>
2024-04-12 11:32:18 +0200

    fixed async notebook issue and switched to httpx

--- pyeed/fetchers/interprorequester.py
+++ pyeed/fetchers/interprorequester.py
@@ -1,6 +1,6 @@
 import asyncio
 from typing import List
-import aiohttp
+import httpx
 
 
 class InterProRequester:
@@ -10,17 +10,40 @@ class InterProRequester:
         self.rate_limit_per_second = rate_limit_per_second
 
     async def make_request(self):
-        async with aiohttp.ClientSession() as session:
+        """
+        Make a request to the InterPro API for each ID in the list of IDs.
+
+        Returns:
+            A list of JSON responses from the InterPro API for each ID.
+
+        Raises:
+            aiohttp.ClientError: If there is an error making the request.
+            asyncio.TimeoutError: If the request times out.
+        """
+
+        async with httpx.AsyncClient() as session:
             tasks = [self.fetch(session, id) for id in self.ids]
             return await asyncio.gather(*tasks)
 
-    async def fetch(self, session, id):
-        url = (
-            "https://www.ebi.ac.uk/interpro/api/entry/all/protein/UniProt/"
-            + id
-            + "?format=json"
-        )
-        async with session.get(url) as response:
-            data = await response.json()
+    async def fetch(self, session: httpx.AsyncClient, id):
+        """
+        Fetches data from the InterPro API for a given ID.
+        """
+        url = f"https://www.ebi.ac.uk/interpro/api/entry/all/protein/UniProt/{id}?format=json"
+        try:
+            response = await session.get(url)
+            if response.status_code == 204:
+                print(f"No InterPro entry found for ID: {id}")
+                return None
+
+            data = response.json()
             await asyncio.sleep(1 / self.rate_limit_per_second)
+
             return data
+
+        except httpx.RequestError as e:
+            print(f"Request error occurred: {e}")
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP status error occurred: {e}")
+        finally:
+            await response.aclose()
