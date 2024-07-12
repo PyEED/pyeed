64bfae582c350684dfe85cb04838635f70241208
Niklas Abraham - INFlux <nikseb57@gmail.com>
2024-07-12 15:39:52 +0000

    added dna fetcher

--- pyeed/core/dnarecord.py
+++ pyeed/core/dnarecord.py
@@ -1,13 +1,18 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, List
 from uuid import uuid4
 
 from lxml.etree import _Element
 from pydantic import PrivateAttr, model_validator
 from pydantic_xml import attr, element
 from sdRDM.base.listplus import ListPlus
+from IPython.display import clear_output
 from sdRDM.tools.utils import elem2dict
+import asyncio
 
-from .sequencerecord import SequenceRecord
+from pyeed.fetch.dnafetcher import DNAFetcher
+
+
+from pyeed.core.sequencerecord import SequenceRecord
 
 
 class DNARecord(
@@ -48,3 +53,52 @@ class DNARecord(
                 self._raw_xml_data[attr] = elem2dict(value)
 
         return self
+    
+
+    @classmethod
+    def get_id(cls, dna_id: str) -> "DNARecord":
+        """
+        This method creates a 'DNARecord' object from a given DNA accession ID.
+
+        Args:
+            dna_id (str): ID of the DNA in NCBI database.
+
+        Returns:
+            DNARecord: 'DNARecord' with information of the corresponding dna_id.
+        """
+
+        import nest_asyncio
+
+        nest_asyncio.apply()
+
+        if isinstance(dna_id, list) and all(isinstance(x, str) for x in dna_id):
+            warnings.warn("For getting multiple sequences by ID use `get_ids` instead.")
+            return cls.get_ids(dna_id)
+
+        sequences = asyncio.run(DNAFetcher(ids=[dna_id]).fetch(quiet=False))[0]
+        clear_output()
+        return sequences
+    
+    @classmethod
+    def get_ids(cls, accession_ids: List[str]) -> List["DNARecord"]:
+        """Creates a list of 'DNARecord' objects from a list of dna accession IDs.
+
+        Returns:
+            List[DNARecord]: A list of 'DNARecord' objects representing the dna sequences found.
+        """
+
+        import nest_asyncio
+
+        nest_asyncio.apply()
+
+        return asyncio.run(
+            DNAFetcher(ids=accession_ids).fetch(force_terminal=False)
+        )
+    
+
+if __name__ == "__main__":
+    dna_record = DNARecord.get_id('AF188200.1')
+
+    print(f"DNA Record: {dna_record}")
+
+    
