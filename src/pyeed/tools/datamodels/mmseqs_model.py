61a189e6be140bfdb3daad96f56a78be92bfcfe0
alacheim <a.lacheim@outlook.de>
2025-01-14 08:08:04 +0000

    Datamodel for Clusters

---
+++ src/pyeed/tools/datamodels/mmseqs_model.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+class Cluster(BaseModel):
+    """
+    Class for storing the cluster information
+    """
+    
+    representative_id: str = Field(
+        ...,
+        description="ID of the representative sequence, representing sequences of a cluster."
+    )
+    
+    represented_ids: List[str] = Field(
+        ...,
+        description="List of IDs for sequences that are represented by the representative sequence in the cluster."
+    )
