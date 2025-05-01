9db90e3f475bdcbc214ce36333edd852c7022cdf
Niklas Abraham GPU <st184432@stud.uni-stuttgart.de>
2025-05-01 14:13:49 +0000

    fixed ruff imports

--- tests/unit/test_dbchat.py
+++ tests/unit/test_dbchat.py
@@ -2,6 +2,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 from neo4j.exceptions import CypherSyntaxError
+
 from pyeed.dbchat import DBChat
 from pyeed.dbconnect import DatabaseConnector
 
