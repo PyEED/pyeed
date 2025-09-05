1af661094813d6d5896ec833fedae3735fa1c9e6
max <max.haeussler@gmail.com>
2025-09-05 22:34:40 +0200

    🪚🪚🪚

--- pyeed/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from .blast import Blast
-from .clustalo import ClustalOmega
-from .mmseqs import MMSeqs
-
-__all__ = ["Blast", "ClustalOmega", "MMSeqs"]
--- pyeed/tools/services.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class ServiceURL(Enum):
-    CLUSTALO = "http://clustalo:5001/align"
-    BLAST = "http://blast:6001/blast"
-    FOLDSEEK = "http://foldseek:7001/foldseek"
-    MMSEQS = "http://mmseqs:8001/easycluster"
